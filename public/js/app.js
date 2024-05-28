1e42605b5d97740bfad684bf319969550c242cc5
MuhammadayubSobirjonov <mr.admiral2011@gmail.com>
2024-05-28 20:06:57 +0500

    ssalom

---
+++ public/js/app.js
@@ -0,0 +1,122 @@
+let resMenu = document.querySelector('#responsive-menu')
+let hamMenu = document.querySelector('#hamburger-menu')
+let closeMenu = document.querySelector('#close-menu')
+
+hamMenu.addEventListener('click', function () {
+  resMenu.classList.add("right-0")
+  resMenu.classList.remove("-right-full")
+})
+
+
+closeMenu.addEventListener('click', function () {
+  resMenu.classList.add("-right-full")
+  resMenu.classList.remove("right-0")
+})
+
+let text = document.querySelectorAll("#text")
+text.forEach(function (item, index) {
+  item.addEventListener('click', function () {
+    text.forEach(function (item, index) {
+      item.style.color = "#00000080"
+    })
+    item.style.color = "#000000"
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  window.onload = function () {
+//   slideMin();
+//   slideMax();
+// }
+
+// const minVals = document.querySelectorAll(".min-val");
+// const maxVals = document.querySelectorAll(".max-val");
+// const priceInputsMin = document.querySelectorAll(".min-input");
+// const priceInputsMax = document.querySelectorAll(".max-input");
+// const minGap = 0;
+// const ranges = document.querySelectorAll(".slider-track");
+
+// minVals.forEach((minVal, index) => {
+//   const maxVal = maxVals[index];
+//   const priceInputMin = priceInputsMin[index];
+//   const priceInputMax = priceInputsMax[index];
+//   const range = ranges[index];
+
+//   const sliderMinValue = parseInt(minVal.getAttribute('min'));
+//   const sliderMaxValue = parseInt(maxVal.getAttribute('max'));
+
+//   minVal.addEventListener('input', () => slideMin(minVal, maxVal, priceInputMin, range, sliderMinValue, sliderMaxValue));
+//   maxVal.addEventListener('input', () => slideMax(minVal, maxVal, priceInputMax, range, sliderMinValue, sliderMaxValue));
+//   priceInputMin.addEventListener('change', () => setMinInput(minVal, maxVal, priceInputMin, range, sliderMinValue, sliderMaxValue));
+//   priceInputMax.addEventListener('change', () => setMaxInput(minVal, maxVal, priceInputMax, range, sliderMinValue, sliderMaxValue));
+
+//   slideMin(minVal, maxVal, priceInputMin, range, sliderMinValue, sliderMaxValue);
+//   slideMax(minVal, maxVal, priceInputMax, range, sliderMinValue, sliderMaxValue);
+// });
+
+// function slideMin(minVal, maxVal, priceInputMin, range, sliderMinValue, sliderMaxValue) {
+//   let gap = parseInt(maxVal.value) - parseInt(minVal.value);
+//   if (gap <= minGap) {
+//       minVal.value = parseInt(maxVal.value) - minGap;
+//   }
+//   priceInputMin.value = '$' + minVal.value;
+//   setArea(minVal, maxVal, range, sliderMinValue, sliderMaxValue);
+// }
+
+// function slideMax(minVal, maxVal, priceInputMax, range, sliderMinValue, sliderMaxValue) {
+//   let gap = parseInt(maxVal.value) - parseInt(minVal.value);
+//   if (gap <= minGap) {
+//       maxVal.value = parseInt(minVal.value) + minGap;
+//   }
+//   priceInputMax.value = '$' + maxVal.value;
+//   setArea(minVal, maxVal, range, sliderMinValue, sliderMaxValue);
+// }
+
+// function setArea(minVal, maxVal, range, sliderMinValue, sliderMaxValue) {
+//   let minValue = parseInt(minVal.value);
+//   let maxValue = parseInt(maxVal.value);
+//   let rangeWidth = sliderMaxValue - sliderMinValue;
+//   let leftPercent = ((minValue - sliderMinValue) / rangeWidth) * 100;
+//   let rightPercent = ((sliderMaxValue - maxValue) / rangeWidth) * 100;
+
+//   range.style.left = leftPercent + "%";
+//   range.style.right = rightPercent + "%";
+// }
+
+// function setMinInput(minVal, maxVal, priceInputMin, range, sliderMinValue, sliderMaxValue) {
+//   let minPrice = parseInt(priceInputMin.value.replace('$', ''));
+//   if (minPrice < sliderMinValue) {
+//       minPrice = sliderMinValue;
+//   }
+//   if (minPrice > parseInt(maxVal.value) - minGap) {
+//       minPrice = parseInt(maxVal.value) - minGap;
+//   }
+//   priceInputMin.value = '$' + minPrice;
+//   minVal.value = minPrice;
+//   slideMin(minVal, maxVal, priceInputMin, range, sliderMinValue, sliderMaxValue);
+// }
+
+// function setMaxInput(minVal, maxVal, priceInputMax, range, sliderMinValue, sliderMaxValue) {
+//   let maxPrice = parseInt(priceInputMax.value.replace('$', ''));
+//   if (maxPrice > sliderMaxValue) {
+//       maxPrice = sliderMaxValue;
+//   }
+//   if (maxPrice < parseInt(minVal.value) + minGap) {
+//       maxPrice = parseInt(minVal.value) + minGap;
+//   }
+//   priceInputMax.value = '$' + maxPrice;
+//   maxVal.value = maxPrice;
+//   slideMax(minVal, maxVal, priceInputMax, range, sliderMinValue, sliderMaxValue);
+// }
+
---
+++ public/js/product.js
@@ -0,0 +1,128 @@
+let resMenu = document.querySelector('#responsive-menu')
+let hamMenu = document.querySelector('#hamburger-menu')
+let closeMenu = document.querySelector('#close-menu')
+
+hamMenu.addEventListener('click', function () {
+  resMenu.classList.add("right-0")
+  resMenu.classList.remove("-right-full")
+})
+
+closeMenu.addEventListener('click', function () {
+  resMenu.classList.add("-right-full")
+  resMenu.classList.remove("right-0")
+})
+
+let top1 = document.querySelectorAll("#top")
+let flex = document.querySelectorAll("#flex")
+
+top1.forEach(function(item ,index){
+  item.addEventListener('click' , function(){
+    flex[index].classList.toggle('aactive')
+  })
+})
+
+// let openR = document.querySelector("#open_range")
+// let inp = document.querySelector("#inp")
+
+// openR.forEach(function(item , index){
+//     item.addEventListener('click' , function(){
+//         inp.classList.toggle("active2")
+//     })
+// })
+// let color = document.querySelector("#color1")
+
+// let array = ["fe"]
+
+// let color2 = document.querySelector("#color2")
+// let color3 = document.querySelector("#color3")
+// let color4 = document.querySelector("#color4")
+// let color5 = document.querySelector("#color5")
+// let color6 = document.querySelector("#color6")
+// let color7 = document.querySelector("#color7")
+// let color8 = document.querySelector("#color8")
+// let color9 = document.querySelector("#color9")
+// let color10 = document.querySelector("#color10")
+// let color11 = document.querySelector("#color11")
+// let color12 = document.querySelector("#color12")
+
+// color1.addEventListener('click' , function(){
+//   color1.classList.remove("bg-[#8434E1]")
+//   color1.classList.add("bg-[white]")
+//   color1.classList.add("border-2")
+//   color1.classList.add("border-[#8434E1]")
+// })
+
+// color2.addEventListener('click' , function(){
+//     color2.classList.remove("bg-black")
+//     color2.classList.add("bg-[white]")
+//     color2.classList.add("border-2")
+//     color2.classList.add("border-black")
+// })
+
+// color3.addEventListener('click' , function(){
+//     color3.classList.remove("bg-[#F35528]")
+//     color3.classList.add("bg-white")
+//     color3.classList.add("border-2")
+//     color3.classList.add("border-[#F35528]")
+// })
+
+// color4.addEventListener('click' , function(){
+//     color4.classList.remove("bg-[#F16F2B]")
+//     color4.classList.add("bg-white")
+//     color4.classList.add("border-2")
+//     color4.classList.add("border-[#F16F2B]")
+// })
+
+// color5.addEventListener('click' , function(){
+//     color5.classList.remove("bg-[#345EFF]")
+//     color5.classList.add("bg-white")
+//     color5.classList.add("border-2")
+//     color5.classList.add("border-[#345EFF]")
+// })
+
+
+// color6.addEventListener('click' , function(){
+//     color6.classList.remove("bg-white")
+//     color6.classList.add("bg-grey")
+//     color6.classList.add("border-2")
+//     color6.classList.add("border-white")
+// })
+
+// color7.addEventListener('click' , function(){
+//     color7.classList.remove("bg-[#D67E3B]")
+//     color7.classList.add("bg-white")
+//     color7.classList.add("border-2")
+//     color7.classList.add("border-[#D67E3B]")
+// })
+
+// color8.addEventListener('click' , function(){
+//     color8.classList.remove("bg-[#48BC4E]")
+//     color8.classList.add("bg-white")
+//     color8.classList.add("border-2")
+//     color8.classList.add("border-[#48BC4E]")
+// })
+
+// color9.addEventListener('click' , function(){
+//     color9.classList.remove("bg-[#FDC761]")
+//     color9.classList.add("bg-white")
+//     color9.classList.add("border-2")
+//     color9.classList.add("border-[#FDC761]")
+// })
+// color10.addEventListener('click' , function(){
+//     color10.classList.remove("bg-[#E4E5E8]")
+//     color10.classList.add("bg-white")
+//     color10.classList.add("border-2")
+//     color10.classList.add("border-[#E4E5E8]")
+// })
+// color11.addEventListener('click' , function(){
+//     color11.classList.remove("bg-[#E08D9D]")
+//     color11.classList.add("bg-white")
+//     color11.classList.add("border-2")
+//     color11.classList.add("border-[#E08D9D]")
+// })
+// color12.addEventListener('click' , function(){
+//     color12.classList.remove("bg-[#3FDEFF]")
+//     color12.classList.add("bg-white")
+//     color12.classList.add("border-2")
+//     color12.classList.add("border-[#3FDEFF]")
+// })
