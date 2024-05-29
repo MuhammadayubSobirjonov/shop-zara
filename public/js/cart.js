e3585bd46c09bbfe806847948526a046e1328bcf
MuhammadayubSobirjonov <mr.admiral2011@gmail.com>
2024-05-29 16:23:49 +0500

    a

--- public/js/cart.js
+++ public/js/cart.js
@@ -3,10 +3,13 @@ let number = document.querySelectorAll("#number")
 let plus = document.querySelectorAll("#plus")
 let cost = document.querySelectorAll("#cost")
 
-minus.addEventListener('click' , function(){
-    number.innerText = Number(number.innerText) -1
+minus.forEach(function (item, index) {
+    item.addEventListener('click', function () {
+        number.innerText = Number(number.innerText) - 1
+    })
 })
-plus.addEventListener('click' , function(){
-    number.innerText = Number(number.innerText) +1
-
+minus.forEach(function (item, index) {
+    plus.addEventListener('click', function () {
+        number.innerText = Number(number.innerText) + 1
+    })
 })
