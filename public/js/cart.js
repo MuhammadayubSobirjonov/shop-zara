961626537d775d6fb66c4e4b74f73b4651bb96c1
MuhammadayubSobirjonov <mr.admiral2011@gmail.com>
2024-05-29 16:11:25 +0500

    s

---
+++ public/js/cart.js
@@ -0,0 +1,4 @@
+let minus = document.querySelectorAll("#minus")
+let number = document.querySelectorAll("#number")
+let plus = document.querySelectorAll("#plus")
+let cost = document.querySelectorAll("#cost")
