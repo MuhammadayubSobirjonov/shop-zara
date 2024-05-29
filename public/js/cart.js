ceb8b125c34a4033bab3ea69b94e3c77d6323de0
MuhammadayubSobirjonov <mr.admiral2011@gmail.com>
2024-05-29 16:15:16 +0500

    x

--- public/js/cart.js
+++ public/js/cart.js
@@ -2,3 +2,5 @@ let minus = document.querySelectorAll("#minus")
 let number = document.querySelectorAll("#number")
 let plus = document.querySelectorAll("#plus")
 let cost = document.querySelectorAll("#cost")
+
+minus.addEvent
