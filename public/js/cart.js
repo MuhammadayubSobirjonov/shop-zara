0fbe66db6d2a6997049b922e229c92e9428150ce
MuhammadayubSobirjonov <mr.admiral2011@gmail.com>
2024-05-29 16:19:50 +0500

    e

--- public/js/cart.js
+++ public/js/cart.js
@@ -3,4 +3,10 @@ let number = document.querySelectorAll("#number")
 let plus = document.querySelectorAll("#plus")
 let cost = document.querySelectorAll("#cost")
 
-minus.addEvent
+minus.addEventListener('click' , function(){
+    number.innerText = Number(number.innerText) -1
+})
+plus.addEventListener('click' , function(){
+    number.innerText = Number(number.innerText) +1
+
+})
